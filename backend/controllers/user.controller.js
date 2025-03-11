3dc5a32314c5d6accc8f0018adf5f85231d9bb08
Sparsh Bansal <sparshbansal787@gmail.com>
2025-03-12 01:05:15 +0530

    redis has been setup

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,6 +1,7 @@
 import userModel from "../models/user.model.js";
 import * as userService from "../services/user.service.js";
 import { validationResult } from "express-validator";
+import redisClient from '../services/redis.service.js';
 
 export const createUserController = async (req, res) => {
   const errors = validationResult(req);
@@ -16,3 +17,34 @@ export const createUserController = async (req, res) => {
     res.status(400).send(error.message);
   }
 };
+
+export const loginController = async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    try{
+        const {email,password}= req.body;
+        const user = await userModel.findOne({email}).select('+password')
+        if(!user){
+            return res.status(401).json({errors:"Invalid email or password"});
+        }
+        const isMatch = await user.isValidPassword(password);
+        if(!isMatch){
+            return res.status(401).json({errors:"Invalid email or password"});
+        }
+        const token = await user.generateJWT();
+        res.status(200).json({user,token});
+
+    }catch (err){
+        res.status(400).send(err.message);
+    }
+}
+
+export const profileController = async (req,res)=>{
+    console.log(req.user);
+    res.status(200).json({user: req.user});
+
+
+
+}
