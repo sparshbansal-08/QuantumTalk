9e392eeca85f6f9e7a4237f141027caab9fe67e2
Sparsh Bansal <sparshbansal787@gmail.com>
2025-03-12 22:59:52 +0530

    Authorization has been completed with logout functionality

--- backend/middleware/auth.middleware.js
+++ backend/middleware/auth.middleware.js
@@ -1,21 +1,24 @@
 import jwt from "jsonwebtoken";
+import redisClient from "../services/redis.service.js";
 
 export const authUser = async (req, res, next) => {
-    try {
-        // Get token from cookie or Authorization header
-        const token = req.cookies.token || req.headers.authorization.split(' ')[1];
+  try {
+    const token = req.cookies.token || req.headers.authorization.split(" ")[1];
 
-        if (!token) {
-            return res.status(401).json({ error: "You are not authenticated" });
-        }
-
-        // Verify the token
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        req.user = decoded;
-        next();
-        
-    } catch (error) {
-        console.error("Auth Middleware Error:", error);
-        res.status(401).json({ error: "Unauthorized User" });
+    if (!token) {
+      return res.status(401).json({ error: "You are not authenticated" });
     }
+    const isBlackListed = await redisClient.get(token);
+    if (isBlackListed) {
+      res.cookie("token", "");
+      return res.status(401).send({ error: "You are Unauthorized" });
+    }
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    console.error("Auth Middleware Error:", error);
+    res.status(401).json({ error: "Unauthorized User" });
+  }
 };
--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -13,15 +13,15 @@ router.post('/register',
     
     userController.createUserController);
 
-    router.post('/login',
+router.post('/login',
         body('email').isEmail().withMessage('Email must be a valid email address'),
         body('password').isLength({min: 3}).withMessage('Password must be at least 3 characters long'),
         userController.loginController);
 
-        router.get('/profile',authMiddleware.authUser, userController.profileController);
+router.get('/profile',authMiddleware.authUser, userController.profileController);
 
 
-
+router.get('/logout',authMiddleware.authUser, userController.logoutController);
 
 
 
