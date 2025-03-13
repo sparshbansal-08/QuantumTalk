78dbb04ff6aeb7ba1a188871cf62704f3320dc03
Sparsh Bansal <sparshbansal787@gmail.com>
2025-03-13 20:04:06 +0530

    routes api completed

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import morgan from "morgan";
 import connect from "./db/db.js";
 import userRoutes from './routes/user.routes.js'
+import projectRoutes from './routes/project.routes.js'
 import cookieParser from 'cookie-parser'
 import cors from 'cors'
 
@@ -18,6 +19,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser())
 
 app.use('/users', userRoutes)
+
+app.use('/projects', projectRoutes)
 
 app.get("/", (req, res) => {
   res.send("Hello World");
---
+++ backend/services/project.service.js
@@ -0,0 +1,19 @@
+import projectModel from '../models/project.model.js';
+
+
+export const createProject = async ({
+    name, userId
+    
+}) => {
+    if (!name){
+        throw new Error('Name is required');
+
+    }
+    if (!userId){
+        throw new Error('User is required');
+    }
+    const project = await projectModel.create({
+        name, users: [userId]
+    });
+    return project;
+}
