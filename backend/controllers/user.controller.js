ff337d3137a6f7cc068dbfebbb81e44fdeb885d5
Sparsh Bansal <127327393+sparshbansal-08@users.noreply.github.com>
2025-03-18 22:43:29 +0530

    Update user.controller.js

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -4,6 +4,7 @@ import { validationResult } from "express-validator";
 import redisClient from '../services/redis.service.js';
 
 export const createUserController = async (req, res) => {
+  
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array() });
