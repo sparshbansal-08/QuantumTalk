e3441c6c7c0b6de3fada9bab68b0ff4c8715149d
Sparsh Bansal <127327393+sparshbansal-08@users.noreply.github.com>
2025-03-26 23:18:06 +0530

    Update Register.jsx

--- frontend/src/screens/Register.jsx
+++ frontend/src/screens/Register.jsx
@@ -22,6 +22,7 @@ const Register = () => {
         console.log(err.response.data);
       });
   }
+  
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-900">
       <div className="bg-gray-800 p-8 rounded-lg shadow-lg w-full max-w-md">
