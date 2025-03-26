880c244f02a9fdb5f1a1d71f5d7f13ca3332c238
Sparsh Bansal <127327393+sparshbansal-08@users.noreply.github.com>
2025-03-26 23:17:19 +0530

    Update user.context.jsx

--- frontend/src/context/user.context.jsx
+++ frontend/src/context/user.context.jsx
@@ -7,6 +7,7 @@ export const UserContext = createContext();
 export const UserProvider = ({ children }) => {
   const [user, setUser] = useState(null);
 
+  
   return (
     <UserContext.Provider value={{ user, setUser }}>
       {children}
