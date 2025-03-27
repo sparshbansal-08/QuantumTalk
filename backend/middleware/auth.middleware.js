4f98164cafcde1f2a04c423329d1885bba4b6c21
Sparsh Bansal <127327393+sparshbansal-08@users.noreply.github.com>
2025-03-27 23:01:09 +0530

    Update auth.middleware.js

--- backend/middleware/auth.middleware.js
+++ backend/middleware/auth.middleware.js
@@ -1,6 +1,7 @@
 import jwt from "jsonwebtoken";
 import redisClient from "../services/redis.service.js";
 
+
 export const authUser = async (req, res, next) => {
   try {
     const token = req.cookies.token || req.headers.authorization.split(" ")[1];
