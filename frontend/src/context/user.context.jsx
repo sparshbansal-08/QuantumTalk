26c2e94fe86a69112caa7195a9175d57f9fb1f26
Sparsh Bansal <127327393+sparshbansal-08@users.noreply.github.com>
2025-03-27 23:03:16 +0530

    Update user.context.jsx

--- frontend/src/context/user.context.jsx
+++ frontend/src/context/user.context.jsx
@@ -3,6 +3,7 @@ import React, { createContext, useState, useContext } from "react";
 // Create a context for the user
 export const UserContext = createContext();
 
+
 // Create a provider component
 export const UserProvider = ({ children }) => {
   const [user, setUser] = useState(null);
