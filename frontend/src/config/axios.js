958e51be276a453d041d9bb791dabf85215677c2
Sparsh Bansal <127327393+sparshbansal-08@users.noreply.github.com>
2025-04-03 23:29:03 +0530

    Update axios.js

--- frontend/src/config/axios.js
+++ frontend/src/config/axios.js
@@ -4,6 +4,7 @@ const axiosInstance = axios.create({
     baseURL: import.meta.env.VITE_API_URL,
     headers: {
         'Authorization': `Bearer ${localStorage.getItem('token')}`,
+        
     },
 })
 
